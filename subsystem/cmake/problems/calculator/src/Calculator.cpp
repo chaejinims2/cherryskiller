a7a16cabfce23f58b4312010eb82c70ea2861922
cjim <chaejinims2@gmail.com>
2025-09-17 15:15:02 +0900

    cmake: bash: update enhance .gitignore, improve bash calculator functions, and implement CMake build script

--- subsystem/cmake/problems/calculator/src/Calculator.cpp
+++ subsystem/cmake/problems/calculator/src/Calculator.cpp
@@ -1,17 +1,17 @@
 #include "Calculator.h"
 
 int Calculator::Add(int a, int b) {
-    return 0;
+    return a + b;
 }
 
 int Calculator::Subtract(int a, int b) {
-    return 0;
+    return a - b;
 }
 
 int Calculator::Multiply(int a, int b) {
-    return 0;
+    return a * b;
 }
 
 double Calculator::Divide(int a, int b) {
-    return 0;
+    return static_cast<double>(a) / b;
 }
--- subsystem/cmake/problems/calculator/src/Calculator.h
+++ subsystem/cmake/problems/calculator/src/Calculator.h
@@ -1,6 +1,7 @@
 #pragma once
 
-struct Sample {
+class Calculator {
+public:
     int Add(int a, int b);
     int Subtract(int a, int b);
     int Multiply(int a, int b);
